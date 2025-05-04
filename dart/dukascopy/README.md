31de283a550e6156df77ea9b78de0d95d0e6f74f
Erhabor-Fona <osaofonaerhabor@gmail.com>
2025-05-04 03:13:11 +0100

    [New]--included candle models

--- dart/dukascopy/README.md
+++ dart/dukascopy/README.md
@@ -14,7 +14,7 @@ Add the package to your `pubspec.yaml`:
 
 ```yaml
 dependencies:
-  dukascopy: ^0.1.4
+  dukascopy: ^0.1.5
   ```
 Then fetch:
 ```bash
--- dart/dukascopy/example/dukascopy_example.dart
+++ dart/dukascopy/example/dukascopy_example.dart
@@ -1,9 +1,10 @@
 import 'package:dukascopy/dukascopy.dart';
 
+
 void main() async {
   // Load instrument groups
   final groups = await fetchInstrumentGroups();
-  log('Groups: ${groups.keys}');
+  print('Groups: ${groups.keys}');
 
   // Fetch 5 days of EUR/USD daily data:
   final start = DateTime.utc(2025, 1, 1);
@@ -15,7 +16,7 @@ void main() async {
     limit: 5,
   );
   dailyRows.forEach((row) {
-    log('Timestamp=${row[0]}, OHLC=[${row[1]},${row[2]},${row[3]},${row[4]}], vol=${row[5]}');
+    print('Timestamp=${row[0]}, OHLC=[${row[1]},${row[2]},${row[3]},${row[4]}], vol=${row[5]}');
   });
 
   // Stream tick data for the next 10 seconds:
@@ -28,6 +29,21 @@ void main() async {
     endMillis: now.add(const Duration(seconds: 10)).millisecondsSinceEpoch,
     limit: 10,
   )) {
-    log('Tick @${tick[0]}: bid=${tick[1]}, ask=${tick[2]}');
+    print('Tick @${tick[0]}: bid=${tick[1]}, ask=${tick[2]}');
   }
+
+final rawRows = await fetch(
+  instrument: 'EUR/USD',
+  interval: '1DAY',
+  offerSide: 'B',
+  lastUpdateMillis: start.millisecondsSinceEpoch,
+  limit: 5,
+);
+
+final candles = rawRows.map((row) => Candle.fromRaw(row)).toList();
+for (var c in candles) {
+  print(c);
+}
+
+
 }
